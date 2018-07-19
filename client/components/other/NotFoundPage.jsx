f31dbf6b7fc2486f2244fae3cc18115ad3ac31d2
Effy <bydlo.vezde@gmail.com>
2018-07-19 11:27:12 +0500

    Добавлен пресет для ноды на сервере

--- client/components/other/NotFoundPage.jsx
+++ client/components/other/NotFoundPage.jsx
@@ -14,15 +14,15 @@ const NotFoundPage = ({ staticContext = {} }) => {
 	staticContext.notFound = true;
 
 	return (
-		<div className={styles['not-found']}>
+		<div className="not-found">
 			<Helmet>
 				<title>Pink | Страница не найдена!</title>
 			</Helmet>
 			<Header className="header" />
 			<main>
-				<section className={styles['not-found__inner']}>
-					<p className={styles['not-found__number']}>404</p>
-					<p className={styles['not-found__description']}>
+				<section className="not-found__inner">
+					<p className="not-found__number">404</p>
+					<p className="not-found__description">
 						Кажется, вы немного не туда попали
 					</p>
 				</section>
--- webpack.server.js
+++ webpack.server.js
@@ -1,28 +1,17 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpackNodeExternals = require('webpack-node-externals');
-const autoprefixer = require('autoprefixer');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const baseConfig = require('./webpack.base.js');
 
 const config = {
 	// говорим вебпаку, что бандл мы делаем не для браузера, а для ноды
 	target: 'node',
-	externals: [nodeExternals()],
 	entry: './src/index.js',
 	// куда поместить выходной файл
 	output: {
 		filename: 'bundle.js',
-		// помещаем в директорию сборки. dirname - текущая директория, в которой мы находимсяю build - конечная папка, которую вебпак сам создаст
 		path: path.resolve(__dirname)
 	},
-	devtool: 'cheap-eval-source-map',
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-			chunkFilename: '[id].css'
-		})
-	],
 	module: {
 		rules: [
 			{
@@ -34,67 +23,8 @@ const config = {
 				// опции для бабель-лодера
 				options: {
 					// какие пресеты мы хотим использовать: react - для JSX, stage-0 - для async-code
-					presets: [
-						'react',
-						'stage-0',
-						[
-							'env',
-							{
-								targets: {
-									browsers: ['last 2 versions', 'not ie < 11']
-								}
-							}
-						]
-					]
+					presets: ['react', 'node8', 'stage-0']
 				}
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							// you can specify a publicPath here
-							// by default it use publicPath in webpackOptions.output
-							publicPath: './public'
-						}
-					},
-					{
-						loader: require.resolve('css-loader/locals'),
-						options: {
-							importLoaders: 2,
-							modules: true,
-							sourceMap: true,
-							localIdentName: '[local]__[hash:3]'
-						}
-					},
-					{
-						loader: require.resolve('postcss-loader'),
-						options: {
-							// Necessary for external CSS imports to work
-							// https://github.com/facebookincubator/create-react-app/issues/2677
-							ident: 'postcss',
-							plugins: () => [
-								require('postcss-flexbugs-fixes'), // eslint-disable-line global-require
-								autoprefixer({
-									browsers: [
-										'>1%',
-										'last 4 versions',
-										'Firefox ESR',
-										'not ie < 11'
-									],
-									flexbox: 'no-2009'
-								})
-							]
-						}
-					},
-					{
-						loader: require.resolve('sass-loader'),
-						options: {
-							sourceMap: false
-						}
-					}
-				]
 			}
 		]
 	},
